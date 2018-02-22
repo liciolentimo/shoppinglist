7aaa258019a128c4348d83b8a7689725e7a4d0a9
Licio Lentimo <lentimo@gmail.com>
2018-02-22 18:42:48 +0300

    Created cursor for sql database

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'shoppinglist'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+#init mysql
+mysql = MySQL(app)
+
 @app.route('/')
 def index():
 	return render_template('home.html')
@@ -27,9 +37,27 @@ class RegisterForm(Form):
 def register():
 		form = RegisterForm(request.form)
 		if request.method == 'POST' and form.validate():
-			return render_template('register.html')
+		    name = form.name.data
+		    email = form.email.data
+		    username = form.username.data
+		    password = sha256_crypt.encrypt(str(form.password.data))
+
+		    cur = mysql.connection.cursor()
+
+		    cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",(name, email, username, password))
+
+		    mysql.connection.commit()
+
+		    #close connection
+		    cur.close()
+
+		    flash('You are now registered and can log in', 'success')
+
+		    redirect(url_for('index'))
+       
 		return render_template('register.html', form=form)	    	
 
 
 if __name__ == '__main__':
+	app.secret_key = '123456'
 	app.run(debug=True)
